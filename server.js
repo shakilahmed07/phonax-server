190ca5282873e4f11fa54c5164e2e375d7c19e6f
SHAKIL AHMED <shakilahmed@SHAKILs-MacBook-Air.local>
2022-09-19 19:23:04 +0600

    testing

--- server.js
+++ server.js
@@ -1,66 +1,56 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const app = express();
 const http = require("http");
-module.exports = { app }
-const mongoose = require('mongoose')
-const cors = require('cors')
+module.exports = { app };
+const mongoose = require("mongoose");
+const cors = require("cors");
 const server = http.createServer(app);
-const port = process.env.PORT || 8000
-const bodyParser = require('body-parser')
+const port = process.env.PORT || 8000;
+const bodyParser = require("body-parser");
 const { Server } = require("socket.io");
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded( { extended: false } ))
-app.use(cors())
-express.json()
-app.use('/uploads', express.static('uploads'))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+express.json();
+app.use("/uploads", express.static("uploads"));
 
 // Routers
-app.use('/products', require('./routers/productRoute'))
-app.use('/petterns', require('./routers/petternRoute'))
-app.use('/create-checkout-session', require('./routers/stripeRoute'))
+app.use("/products", require("./routers/productRoute"));
+app.use("/petterns", require("./routers/petternRoute"));
+app.use("/create-checkout-session", require("./routers/stripeRoute"));
 
-const io = new Server(server, {
-    cors: {
-      origin: "http://localhost:3000",
-      methods: ["GET", "POST"],
-    },
-  });
-  
-  
-  io.on("connection", (socket) => {
-    console.log(`User Connected: ${socket.id}`);
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
   // audio call
-    socket.emit('me',socket.id)
-  
-    socket.on('join_room',(data)=>{
-        socket.join(data);
-        console.log(`user id  :  ${socket.id} joined room : ${data}`)
-  
-    });
-    socket.on("send_message",(data)=>{
-        console.log(data)
-        socket.to(data.room).emit('receive_message',data);
-  
-    });
- 
+  socket.emit("me", socket.id);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`user id  :  ${socket.id} joined room : ${data}`);
   });
+  socket.on("send_message", (data) => {
+    console.log(data);
+    socket.to(data.room).emit("receive_message", data);
+  });
+});
 
+const mongodb_uri = process.env.MONGODB_URI;
 
-
-const mongodb_uri = process.env.MONGODB_URI
-
-
-mongoose.connect(mongodb_uri).then(() => {
+mongoose
+  .connect(mongodb_uri)
+  .then(() => {
     try {
-        server.listen(port, () => {
-            console.log(`server is running on port: ${port}`)
-        })
+      server.listen(port, () => {
+        console.log(`server is running on port: ${port}`);
+      });
     } catch (error) {
-        console.log(error)
+      console.log(error);
     }
-}).catch(error => {
-    console.log(error)
-})
-
+  })
+  .catch((error) => {
+    console.log(error);
+  });
